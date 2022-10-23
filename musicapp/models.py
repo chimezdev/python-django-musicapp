5221a9a40d343b6e90e82691053d4616ba31c46a
chimezdev <ochuba6@gmail.com>
2022-10-23 09:32:18 +0100

    linking and getting data from database

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
 # Create your models here.
-class Abount:
-    id: int
-    name: str
-    details: str
+class About(models.Model): #converts the class into a model
+    #id is authomatically generated 
+    name = models.CharField(max_length=50) #charfield for 'str' type
+    details = models.CharField(max_length=500)
+    
     
--- musicapp/views.py
+++ musicapp/views.py
@@ -1,22 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Abount #imported for use here
+from .models import About #imported for use here
 
 # Create your views here.
 #define whateever you wish to be rendered here
 
 def index(request):
-    about1 = Abount() #instantiating the class
-    about1.id = 1
-    about1.name = "Stone"
-    about1.details = "A 3D designer turned Cloud/DevOps Engineer. I love helping organizations save cost and time by focusing more on their business logics, less on infrastructure management, minimize errors through task automation using Continuous Integration and Continuous Deployment – CICD pipeline. I recently developed and automated the deployment of a microservice CRUD web application that lets users share images to their feed."
     
+    about = About.objects.all();
+        
     # context = { #this will b gotten from our db later
     #     'name': 'Stone',
     #     'age': '28',
     #     'nationality': 'Nigerian'
     # }
-    return render(request, 'index.html', {'about': about1}) #include ',context' to the return when you uncomment the context dictionary abv
+    return render(request, 'index.html', {'about': about}) #include ',context' to the return when you uncomment the context dictionary abv
 
 
 def counter(request):
